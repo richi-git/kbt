a559b8a9330f8f03209a269358a55d5e1b95b7e6
evnnxx53 <evnnxx10@gmail.com>
2026-06-07 19:24:19 +0800

    Add files via upload

--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
 file(TO_CMAKE_PATH "C:\\Users\\rzkyv\\Develop\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\Users\\rzkyv\\Downloads\\kbt (2)\\kbt" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\Users\\rzkyv\\Downloads\\kbt-main" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -11,14 +11,14 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
   "FLUTTER_ROOT=C:\\Users\\rzkyv\\Develop\\flutter"
-  "PROJECT_DIR=C:\\Users\\rzkyv\\Downloads\\kbt (2)\\kbt"
+  "PROJECT_DIR=C:\\Users\\rzkyv\\Downloads\\kbt-main"
   "FLUTTER_ROOT=C:\\Users\\rzkyv\\Develop\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\rzkyv\\Downloads\\kbt (2)\\kbt\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\Users\\rzkyv\\Downloads\\kbt (2)\\kbt"
-  "FLUTTER_TARGET=C:\\Users\\rzkyv\\Downloads\\kbt (2)\\kbt\\lib\\main.dart"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\rzkyv\\Downloads\\kbt-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\rzkyv\\Downloads\\kbt-main"
+  "FLUTTER_TARGET=C:\\Users\\rzkyv\\Downloads\\kbt-main\\lib\\main.dart"
   "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuOQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MDBiMGM5MWYwNg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NDJkM2Q3NWE1Ng==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS41"
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=C:\\Users\\rzkyv\\Downloads\\kbt (2)\\kbt\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=C:\\Users\\rzkyv\\Downloads\\kbt-main\\.dart_tool\\package_config.json"
 )
